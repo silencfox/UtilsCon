2ae4421acef8326ad20ef360f6b679d8fb10f080
kalcala <the-punisher_01@hotmail.com>
2024-01-20 19:13:57 -0400

    Creacion de proyecto
    Se crea proyecto con menu, clase de calculadora y switch

---
+++ Calcular.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilesCon
+{
+    class Calcular
+    {
+        public int Suma(int val1, int val2) {
+            int result;
+
+            result=val1 + val2;
+            return result;
+        }
+
+        public int restar(int val1, int val2) {
+            int result;
+            result =val1 - val2;
+            return result;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿namespace UtilesCon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcular calcular = new Calcular();
+
+            int val1 = 5, val2 = 3;
+
+            Console.WriteLine(calcular.Suma(val1, val2));
+            string resp="";
+            while (resp != "0")
+            {
+                Console.WriteLine("******  ELIGE UNA OPCION  ******");
+                Console.WriteLine("******  1.- OPCION  ******");
+                Console.WriteLine("******  2.- SUMAR  ******");
+                Console.WriteLine("******  3.- OPCION  ******");
+                Console.WriteLine("******  4.- OPCION  ******");
+                Console.WriteLine("******  5.- OPCION  ******");
+                Console.WriteLine("******  6.- OPCION  ******");
+                Console.WriteLine("******  7.- OPCION  ******");
+                Console.WriteLine("******  8.- OPCION  ******");
+                Console.WriteLine("******  9.- OPCION  ******");
+
+                Console.Write("Elige una opcion: ");
+                resp= Console.ReadLine();
+
+                switch (resp)
+                {
+                    case "1":
+                        Console.Write($"La opcion selecionada es {resp}");
+                        break; 
+
+                    case "2":
+                        Calcular calcular1 = new Calcular();
+                        Console.WriteLine("ingrese el primer valor ");
+                        Console.WriteLine(calcular1.Suma(val1, val2));
+                        calcular1.Suma();
+                        break; 
+                    case "3":
+                    
+                        break; 
+                    case "4":
+                            break; 
+                    case "5":
+                    
+                        break; 
+                    case "6":
+                    
+                        break;
+
+
+
+
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+
+
+}
