b611bb63e8ace13a1cca36fcc2ef8328211d266a
kalcala <the-punisher_01@hotmail.com>
2024-01-23 01:03:59 -0400

    Agregando clase vehiculo

---
+++ Vehiculo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilesCon
+{
+    internal class Vehiculo
+    {
+        private int _puertas;
+        private string _traccion;
+        private string _tipo;
+        private int _pasajeros;
+
+
+        public int Puertas { get => _puertas; set => _puertas = value; }
+        public string Traccion { get => _traccion; set => _traccion = value; }
+        public string Tipo { get => _tipo; set => _tipo = value; }
+        public int Pasajeros { get => _pasajeros; set => _pasajeros = value; }
+
+        public void carro() {
+        
+        }
+
+        public void camion() {
+        
+        }
+
+        public void camioneta() {
+        
+        }
+
+        public void jeep() { 
+        
+        }
+
+    }
+}
